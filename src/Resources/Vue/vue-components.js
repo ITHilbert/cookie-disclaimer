8159fa0a2f0eed466632ce4b2dd396efecb8c789
Jörg Hilbert <hilbert@it-entwickler-hilbert.de>
2022-08-05 16:13:37 +0200

    fix 1

--- src/Resources/Vue/vue-components.js
+++
@@ -1,4 +0,0 @@
-//Cookie Disclaimer
-Vue.component('cookie-disclaimer', require('./cookies-disclaimer.vue').default);
-Vue.component('cookies-allow-reset', require('./cookies-allow-reset.vue').default);
-Vue.component('cookies-infos', require('./cookies-infos.vue').default);
